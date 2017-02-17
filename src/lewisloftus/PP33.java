5bd5f5d94d5d4335a99d2c30985366ea2bb8c957
shaggy0798 <david.freer@gmail.com>
2017-02-17 14:23:08 -0500

    Creating and printing a random phone number with the zip not allowed to have an 8 or 9.

--- src/lewisloftus/PP33.java
+++ src/lewisloftus/PP33.java
@@ -15,9 +15,28 @@ public class PP33 {
 
     public static void main(String[] args) {
         Random r = new Random(System.currentTimeMillis());
-        int zip = r.nextInt(599) + 100;
-        int nextThree = r.nextInt(899) + 100;
-        int lastFour = r.nextInt(8999) + 1000;
-        System.out.println(zip + "-" + nextThree + "-" + lastFour);
+        int zip = r.nextInt(7);
+        int zip1 = r.nextInt(7);
+        int zip2 = r.nextInt(7);
+        String zipCode = "" + zip + "" + zip1 + "" + zip2;
+        int nextThree = r.nextInt(999);
+        int lastFour = r.nextInt(9999);
+        System.out.print(zipCode + "-");
+        if(nextThree < 10)
+            System.out.print("00" + nextThree+ "-");
+        else if(nextThree < 100)
+            System.out.print("0" + nextThree+ "-");
+        else
+            System.out.print(nextThree+ "-");
+        if(lastFour < 10){
+            System.out.print("000" + lastFour);
+        }
+        else if(lastFour < 100)
+            System.out.print("00" + lastFour);
+        else if(lastFour < 1000)
+            System.out.print("0" + lastFour);
+        else
+            System.out.print(lastFour);
+
     }
 }
